447e81c4c8013698b2623ed4babf0c86812b4e20
hengrumay <hengrumay@users.noreply.github.com>
2016-08-23 01:19:09 -0400

    Update markov.py

--- python/markov.py
+++ python/markov.py
@@ -12,7 +12,7 @@
 # A possible output would be:
 
 # > show himself once more than the universe and what I often catch myself playing our well-connected game went on. 
-#Our friend was absolutely correct: nobody from the group needed this way. We never been as the Earth has the network of eternity.
+# > Our friend was absolutely correct: nobody from the group needed this way. We never been as the Earth has the network of eternity.
 
 # There are design choices to make; feel free to experiment and shape the program as you see fit. 
 #Jeff Atwood's [Markov and You](http://blog.codinghorror.com/markov-and-you/) is a fun place to get started 
